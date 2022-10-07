31da7b90728d705a57e2a489638c2f576624fd55
Ilaz Gabdrakhmanov <ilnaz@example.com>
2022-10-07 23:24:48 +0300

    feat: added opportunity to connect with username and password

--- conn.go
+++ conn.go
@@ -3,6 +3,8 @@ package httprateredis
 type Config struct {
 	Host      string `toml:"host"`
 	Port      uint16 `toml:"port"`
+	Username  string `toml:"username"`
+	Password  string `toml:"password"`
 	DBIndex   int    `toml:"db_index"`   // default 0
 	MaxIdle   int    `toml:"max_idle"`   // default 4
 	MaxActive int    `toml:"max_active"` // default 8
